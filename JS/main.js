fbe0bbe670fb1c532aa924e98662a7a3d0fe273c
alex mejias <amejias101@qc.cuny.edu>
2012-12-03 00:26:16 -0500

    init. works

---
+++ JS/main.js
@@ -0,0 +1,51 @@
+(function () {
+window. App = {
+	M: {},
+	V: {},
+	C: {}
+}
+
+App.M.Station = Backbone.Model.extend({});
+
+App.C.Branch = Backbone.Collection.extend({
+	model: App.M.Station
+});
+
+App.V.Branch = Backbone.View.extend({
+	tagName: 'ul',
+
+	render: function (){
+		this.collection.each(this.addOne, this);
+	},
+
+	addOne: function (station){
+		var stationView = new App.V.Station({model: station});
+		this.$el.append(stationView.render().el);
+	}
+});
+
+App.V.Station = Backbone.View.extend({
+	tagName: 'li',
+
+	render: function (){
+		this.$el.html(this.model.get('title'));
+		return this;
+	}
+
+});
+
+var stations = new App.C.Branch([
+	{
+		title: 'oside',
+		zone: 1
+	},
+	{
+		title: 'lyn',
+		zone: 3
+	}
+]);
+
+var branchView = new App.V.Branch({collection: stations});
+branchView.render();
+$(document.body).html(branchView.el);
+}) (); // end shell
